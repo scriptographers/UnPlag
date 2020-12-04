2ae47a063b225117e5b16b508c68ec9e73185ebe
sudoRicheek <richeekdas2001@gmail.com>
2020-12-05 00:30:34 +0530

    fixed admin and returned pastchecks

--- unplag/organization/admin.py
+++ unplag/organization/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Organization
 
 # Register your models here.
+class OrganizationAdmin(admin.ModelAdmin):
+    readonly_fields = ('name',)
+    list_display = ('name', 'title', 'date_created', 'unique_code')
 
-admin.site.register(Organization)
+admin.site.register(Organization, OrganizationAdmin)
