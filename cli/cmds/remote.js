30fd7e65554dc8280a5f5902e3a39ac32bb7e623
devansh-dvj <devansh.dvj@gmail.com>
2020-12-07 23:04:07 +0530

    Added yargs parser to cli

---
+++ cli/cmds/remote.js
@@ -0,0 +1,7 @@
+exports.command = 'remote <command>'
+exports.desc = 'Manage set of tracked repos'
+exports.builder = function (yargs) {
+  return yargs.commandDir('remote_cmds')
+}
+exports.handler = function (argv) {
+}
